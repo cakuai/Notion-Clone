73b1a2504876ddc4473c61e61a21d2812cb26f00
Cathy Kuai <ckuai@CathydeMacBook-Air.local>
2024-02-04 02:05:25 +0800

    second

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-emerald-500">
+        Click me!!!
+      </Button>
+    </div>
+  )
+}
